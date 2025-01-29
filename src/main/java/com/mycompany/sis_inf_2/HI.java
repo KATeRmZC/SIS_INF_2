0e678316ce47885bb7efea87f4c82d53f2c4fe45
125Srh <104373181+125Srh@users.noreply.github.com>
2025-01-29 10:12:01 -0400

    Revert "ventanas del Producto (Detalle,Tipo,Material,Precio,etc..)"

---
+++ src/main/java/com/mycompany/sis_inf_2/HI.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sis_inf_2;
+
+/**
+ *
+ * @author ECTOR
+ */
+public class HI {
+    public static void main(String[] args) {
+        System.out.println("I forget everthing, I won't be able to remember any word that you said.");
+    
+    }
+    
+    
+    
+    
+    
+}
